cffa8b1b5d6aead476480a7e0301fc66d47028bd
Taavi Väänänen <hi@taavi.wtf>
2021-03-23 15:30:27 +0200

    add sqwiki and sqwikiquote usernames
    Change-Id: Ia4baebc08411f7a7b6708b753a1783bfa00c9e0c

--- user-config.py
+++ user-config.py
@@ -5,8 +5,9 @@ global usernames
 usernames["vagrant"]["en"] = "MajavahBotti"
 usernames["wikipedia"]["en"] = "MajavahBot"
 usernames["wikipedia"]["fi"] = "MajavahBot"
+usernames["wikipedia"]["sq"] = "MajavahBot"
+usernames["wikiquote"]["sq"] = "MajavahBot"
 usernames['wikidata']['wikidata'] = u'MajavahBot'
 usernames['meta']['meta'] = u'MajavahBot'
 
 password_file = "user-password.py"
-# verbose_output = True
