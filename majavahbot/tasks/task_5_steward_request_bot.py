1b80955b93341d3c25f65ea37a15d76ae74b0cb9
Taavi Väänänen <hi@taavi.wtf>
2022-12-06 21:50:56 +0200

    task5: archive old requests
    Change-Id: Ibd605549d28e881574049f49a054334915841b7c

--- majavahbot/tasks/task_5_steward_request_bot.py
+++ majavahbot/tasks/task_5_steward_request_bot.py
@@ -1,4 +1,5 @@
 import ipaddress
+from datetime import datetime, timezone
 
 import mwparserfromhell
 from pywikibot.data.api import QueryGenerator
@@ -26,7 +27,7 @@ class StewardRequestTask(Task):
             return None
 
         if not was_enough_time_ago(
-            data[0]["timestamp"], self.get_task_configuration("time_min")
+            data[0]["timestamp"], self.get_task_configuration("mark_done_min_time")
         ):
             return None
 
@@ -55,18 +56,110 @@ class StewardRequestTask(Task):
                 return None
 
         if not was_enough_time_ago(
-            entry["timestamp"], self.get_task_configuration("time_min")
+            entry["timestamp"], self.get_task_configuration("mark_done_min_time")
         ):
             return None
 
         return data[0]["user"]
 
+    def process_srp_section(self, api, section):
+        header = section.filter_headings()[0]
+        if not header:
+            return False  # ???
+
+        status = None
+
+        accounts = set()
+        ips = set()
+
+        awesome_people = set()
+
+        for template in section.filter_templates():
+            if template.name.matches("status"):
+                status = template
+            elif (
+                template.name.matches("LockHide")
+                or template.name.matches("MultiLock")
+                or template.name.matches("Multilock")
+                or template.name.matches("Luxotool")
+                or template.name.matches("MultiLockHide")
+            ):
+                for param in template.params:
+                    if not param.can_hide_key(param.name):
+                        continue
+                    param_text = param.value.strip_code().strip()
+                    if len(param_text) == 0:
+                        continue
+
+                    validate_text = param_text
+                    if validate_text.count("/") == 1:
+                        first, second = validate_text.split("/")
+
+                        # if the part after the slash is numeric, check if the part before is an ip
+                        # so CIDR ranges are checked if they are globally blocked instead of locked
+                        if second.isdigit():
+                            # since some people suggest blocking ranges larger than /16, don't crash
+                            second = int(second)
+                            if second > 16:
+                                validate_text = first
+
+                    try:
+                        ipaddress.ip_address(validate_text)
+                        ips.add(param_text)
+                    except ValueError:
+                        accounts.add(param_text)
+
+        if not status:
+            return False
+        if status.has(1):
+            return len(status.get(1).value) > 0 and status.get(1).value != "onhold"
+
+        if ("unlock" in header and "/unlock" not in header) or (
+            "unblock" in header and "/unblock" not in header
+        ):
+            print(f"Assuming section {header} is a un(b)lock request, skipping")
+            return False
+
+        mark_done = True
+
+        for ip in ips:
+            steward = self.get_steward_who_gblocked_ip(api, ip)
+            if steward is None:
+                mark_done = False
+            else:
+                awesome_people.add(steward)
+
+        for account in accounts:
+            steward = self.get_steward_who_locked_account(api, account)
+            if steward is None:
+                mark_done = False
+            else:
+                awesome_people.add(steward)
+
+        print(accounts, ips, awesome_people, mark_done)
+        if not mark_done or len(awesome_people) == 0:
+            return False
+
+        # remove duplicates
+        awesome_people = ", ".join(sorted(awesome_people))
+
+        if mark_done:
+            status.add(1, "done")
+            section.append(
+                ": '''Robot clerk note:''' {{done}} by " + awesome_people + ". ~~~~\n"
+            )
+            print("Marking as done", awesome_people, status, ips, accounts)
+
+        return False
+
     def run(self):
         self.merge_task_configuration(
             run=True,
             srg_page="Steward requests/Global",
-            summary="BOT: Marking done requests as done",
-            time_min=5 * 60,
+            summary="Bot clerking",
+            mark_done_min_time=5 * 60,
+            archive_page_format="Steward requests/Global/{year}-w{week}",
+            archive_min_time=60 * 60,
         )
 
         if self.get_task_configuration("run") is not True:
@@ -74,113 +167,68 @@ class StewardRequestTask(Task):
             return
 
         api = self.get_mediawiki_api()
-        page = api.get_page(self.get_task_configuration("page"))
-        page_text = page.get(force=True)
-        parsed = mwparserfromhell.parse(page_text)
+
+        request_page = api.get_page(self.get_task_configuration("srg_page"))
+        request_original_text = request_page.get(force=True)
+
+        parsed = mwparserfromhell.parse(request_page.get(force=True))
         sections = parsed.get_sections(levels=[3])
 
+        to_archive = ""
+
         for section in sections:
-            header = section.filter_headings()[0]
-            if ("unlock" in header and "/unlock" not in header) or (
-                "unblock" in header and "/unblock" not in header
-            ):
-                print("Assuming section", header, " is a un(b)lock request, skipping")
+            is_complete = self.process_srp_section(api, section)
+
+            if not is_complete:
                 continue
 
-            status = None
-
-            accounts = []
-            ips = []
-
-            awesome_people = []
-
-            for template in section.filter_templates():
-                if template.name.matches("status"):
-                    status = template
-                elif (
-                    template.name.matches("LockHide")
-                    or template.name.matches("MultiLock")
-                    or template.name.matches("Multilock")
-                    or template.name.matches("Luxotool")
-                    or template.name.matches("MultiLockHide")
-                ):
-                    for param in template.params:
-                        if not param.can_hide_key(param.name):
-                            continue
-                        param_text = param.value.strip_code().strip()
-                        if len(param_text) == 0:
-                            continue
-
-                        validate_text = param_text
-                        if validate_text.count("/") == 1:
-                            first, second = validate_text.split("/")
-
-                            # if the part after the slash is numeric, check if the part before is an ip
-                            # so CIDR ranges are checked if they are globally blocked instead of locked
-                            if second.isdigit():
-                                # since some people suggest blocking ranges larger than /16, don't crash
-                                second = int(second)
-                                if second > 16:
-                                    validate_text = first
-
-                        try:
-                            ipaddress.ip_address(validate_text)
-                            ips.append(param_text)
-                        except ValueError:
-                            accounts.append(param_text)
-
-            # status already has a value, assuming this has already been processed
-            if (not status) or (status.has(1) and len(status.get(1).value) > 0):
-                print("Section has non-ok status", status, accounts, ips)
+            last_reply = api.get_last_reply(str(section))
+            if last_reply is None:
+                continue
+            if (
+                datetime.now(tz=timezone.utc) - last_reply
+            ).total_seconds() <= self.get_task_configuration("archive_min_time"):
                 continue
 
-            mark_done = True
-
-            for ip in ips:
-                steward = self.get_steward_who_gblocked_ip(api, ip)
-                if steward is None:
-                    mark_done = False
-                else:
-                    awesome_people.append(steward)
-
-            for account in accounts:
-                steward = self.get_steward_who_locked_account(api, account)
-                if steward is None:
-                    mark_done = False
-                else:
-                    awesome_people.append(steward)
-
-            print(accounts, ips, awesome_people, mark_done)
-            if not mark_done or len(awesome_people) == 0:
-                continue
-
-            # remove duplicates
-            awesome_people = ", ".join(list(dict.fromkeys(awesome_people)))
-
-            if mark_done:
-                status.add(1, "done")
-                section.append(
-                    ": '''Robot clerk note:''' {{done}} by "
-                    + awesome_people
-                    + ". ~~~~\n"
-                )
-                print("Marking as done", awesome_people, status, ips, accounts)
+            print(f"Archiving section {section}")
+            to_archive += "\n" + str(section)
+            parsed.replace(section, "")
 
         new_text = str(parsed)
         new_text = remove_empty_lines_before_replies(new_text)
 
         if (
-            new_text != page_text
+            new_text != request_original_text
             and self.should_edit()
             and (not self.is_manual_run or confirm_edit())
         ):
             api.site.login()
-            page.text = new_text
-            page.save(
+            request_page.text = new_text
+            request_page.save(
                 self.get_task_configuration("summary"),
                 botflag=self.should_use_bot_flag(),
             )
             self.record_trial_edit()
+
+            if to_archive != "":
+                now = datetime.now()
+                archive_page_name = self.get_task_configuration(
+                    "archive_page_format"
+                ).format(
+                    year=now.year,
+                    week=now.isocalendar().week,
+                )
+
+                archive_page = api.get_page(archive_page_name)
+                if archive_page.exists():
+                    archive_page.text = archive_page.get(force=True) + "\n" + to_archive
+                else:
+                    archive_page.text = to_archive
+
+                archive_page.save(
+                    self.get_task_configuration("summary"),
+                    botflag=self.should_use_bot_flag(),
+                )
 
 
 task_registry.add_task(StewardRequestTask(5, "Steward request bot", "meta", "meta"))
