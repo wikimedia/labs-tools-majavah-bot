9c7db79f77b36d9cad1520b8c83766942ba372c7
Taavi Väänänen <hi@taavi.wtf>
2023-07-28 17:22:22 +0300

    user-config: add testwiki username
    Change-Id: Ibfb52d1103255d7ee480134fa5d25cca9f47f8ba

--- user-config.py
+++ user-config.py
@@ -9,6 +9,7 @@ usernames["wikipedia"]["en"] = "MajavahBot"
 usernames["wikipedia"]["fi"] = "MajavahBot"
 usernames["wikipedia"]["sq"] = "MajavahBot"
 usernames["wikiquote"]["sq"] = "MajavahBot"
+usernames["wikipedia"]["test"] = "MajavahBot"
 usernames["wikidata"]["wikidata"] = "MajavahBot"
 usernames["meta"]["meta"] = "MajavahBot"
 
