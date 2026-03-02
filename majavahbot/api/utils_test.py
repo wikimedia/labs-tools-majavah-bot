2932a1cc3915000b6232debad7e78ea9e506a733
Taavi Väänänen <hi@taavi.wtf>
2026-03-02 21:07:39 +0200

    Format with latest Black
    Change-Id: Id8ac8aa75bce2296ea98a8bc384dde61d1ec9f45

--- majavahbot/api/utils_test.py
+++ majavahbot/api/utils_test.py
@@ -3,16 +3,14 @@ from majavahbot.api.utils import remove_comments
 
 def test_remove_comments() -> None:
     assert (
-        remove_comments(
-            """// foo: bar
+        remove_comments("""// foo: bar
 bar
 baz // baz
 // baz
 foo
     // baz
 wee
-"""
-        )
+""")
         == """
 bar
 baz // baz
